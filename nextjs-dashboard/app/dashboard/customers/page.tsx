ead4865a9765d9994cbe58ece51f3db4f9d572de
Najib <52837430+najchris11@users.noreply.github.com>
2024-06-19 21:40:11 -0400

    Uhm, covered the layout as well as routing and conditional css

---
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from "react";
+class Page extends React.Component{
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return <p>Customers Page</p>
+    }
+} export default Page;
+//Uhhh i wanted to make sure I had a grasp of Class based components but that was a bad idea. why is it so much more complex
