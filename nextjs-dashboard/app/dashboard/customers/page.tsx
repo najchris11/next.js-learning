c58e00c81817f04be828aeaa6b5bda8d3108aac8
Najib <52837430+najchris11@users.noreply.github.com>
2024-06-19 21:48:37 -0400

    i had an error bc idk how ts works

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from "react";
 class Page extends React.Component{
-    constructor(props) {
+    constructor(props: {} | Readonly<{}>) {
         super(props)
     }
     render() {
